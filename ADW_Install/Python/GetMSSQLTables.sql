8c1f075c5a6a7445d96040e594c832fa32230028
Paul Coward <97645474+PaulCoward53@users.noreply.github.com>
2022-01-18 18:31:38 -0700

    Delete ADW_Install/Python directory

--- ADW_Install/Python/GetMSSQLTables.sql
+++
@@ -1,9 +0,0 @@
-select 'Insert into ADW_ETL_STAGE (APP_ID,ETL_TYPE,ETL_TYPE_PARM,ETL_SRC_SCHEMA,ETL_SRC_TABLE) 
-        values (''MSAP'',''MSINSERT'',''MSAP_PROD'',''' + TABLE_SCHEMA + ''',''' + table_name + '''); '
- FROM INFORMATION_SCHEMA.tables
-      WHERE TABLE_CATALOG = 'MSAP' and TABLE_SCHEMA = 'dbo'
-
-	  select 'Insert into ADW_ETL_STAGE (APP_ID,ETL_TYPE,ETL_TYPE_PARM,ETL_SRC_SCHEMA,ETL_SRC_TABLE) 
-        values (''NW'',''MSINSERT'',''NW_PROD'',''' + TABLE_SCHEMA + ''',''' + table_name + '''); '
- FROM INFORMATION_SCHEMA.tables
-      WHERE TABLE_CATALOG = 'Northwind' and TABLE_SCHEMA = 'dbo'
